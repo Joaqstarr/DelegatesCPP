63aeea9d44abed7af23abfff947bfef9fb5aa205
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-09-19 20:06:45 -0400

    Convert template methods to inline

--- Delegate.h
+++ Delegate.h
@@ -7,13 +7,33 @@ namespace Events {
 	class Delegate {
 
 	public:
-		void Invoke(params...args);
+		void Invoke(params...args)
+		{
+			for (unsigned int i = 0; i < callbacks.size(); ++i) {
+				callbacks[i](args...);
+			}
+		}
+		void operator+=(std::function<void(params...)> callback) 
+		{
+			Subscribe(callback);
+		}
+		void operator-=(std::function<void(params...)> callback)
+		{
+			Unsubscribe(callback);
+		}
+		void Subscribe(std::function<void(params...)> callback)
+		{
+			callbacks.push_back(callback);
 
-		void operator+=(std::function<void(params...)> callback);
-		void operator-=(std::function<void(params...)> callback);
-		void Subscribe(std::function<void(params...)> callback);
-		void Unsubscribe(std::function<void(params...)> callback);
-
+		}
+		void Unsubscribe(std::function<void(params...)> callback)
+		{
+			for (int i = 0; i < callbacks.size(); ++i) {
+				if (callbacks[i] == callback) {
+					callbacks.erase(i);
+				}
+			}
+		}
 
 	private:
 		std::vector<std::function<void(params...)>> callbacks;
