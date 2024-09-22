ee494bcd6fdd2ceec81da7c9fe36f0a5672ab13f
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-09-22 17:23:20 -0400

    Change to callback id system

--- Delegate.h
+++ Delegate.h
@@ -1,45 +1,56 @@
 #pragma once
 #include <vector>
 #include <functional>
+#include <utility>
+
 namespace Events {
 
 	template<class... params>
 	class Delegate {
 
 	public:
+		using CallbackId = int;
+
 		void Invoke(params...args)
 		{
 			for (unsigned int i = 0; i < callbacks.size(); ++i) {
-				callbacks[i](args...);
+				callbacks[i].second(args...);
 			}
-		}
-		void operator+=(std::function<void(params...)> callback) 
-		{
-			Subscribe(callback);
 		}
 		void operator()(params...args)
 		{
 			Invoke(args...);
 		}
+
+		CallbackId operator+=(std::function<void(params...)> callback)
+		{
+			return Subscribe(callback);
+		}
+
 		void operator-=(std::function<void(params...)> callback)
 		{
 			Unsubscribe(callback);
 		}
-		void Subscribe(std::function<void(params...)> callback)
+		CallbackId Subscribe(std::function<void(params...)> callback)
 		{
-			callbacks.push_back(callback);
+			callbacks.emplace_back(++nextId, std::move(callback));
+			return nextId;
 
 		}
-		void Unsubscribe(std::function<void(params...)> callback)
+		void Unsubscribe(CallbackId id)
 		{
-			for (int i = 0; i < callbacks.size(); ++i) {
-				if (callbacks[i] == callback) {
-					callbacks.erase(i);
-				}
-			}
+			callbacks.erase(
+				std::remove_if(callbacks.begin(), callbacks.end(),
+					[id](const std::pair<CallbackId, std::function<void(params...)>>& p) {
+						return p.first == id;
+					}),
+				callbacks.end()
+			);
 		}
 
 	private:
-		std::vector<std::function<void(params...)>> callbacks;
+		std::vector<std::pair<CallbackId, std::function<void(params...)>>> callbacks;
+		CallbackId nextId = 0;
+
 	};
 }
